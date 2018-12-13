9b642d5fd34e8eb41ee551767800253ec116a562
Tom Hutton <thomaswkhutton@gmail.com>
2018-12-13 14:30:37 -0500

    Setting environmental variables

--- app.js
+++ app.js
@@ -46,5 +46,8 @@ app.get('/results', (req, res) => {
 
 
 app.listen(8080, () => {
-  console.log("The app has started on port 8080!");
-})
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log(`Example app listening at http://${host}:${port}`);
+});
