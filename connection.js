78ca9461a93a87c4361605e344d9a75b86f82af3
Tom Hutton <thomaswkhutton@gmail.com>
2018-12-13 14:42:20 -0500

    Test.

--- connection.js
+++ connection.js
@@ -1,15 +1,15 @@
-const mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "naics"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-module.exports = {con};
+// const mysql = require('mysql');
+//
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "naics"
+// });
+//
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+//
+// module.exports = {con};
