f73b7683f88b7517001b1f6089c964507c4528bb
Tom Hutton <thomaswkhutton@gmail.com>
2018-12-13 15:10:18 -0500

    test

--- connection.js
+++ connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "hcAbtg0uguH4anrH",
-  database: "naics"
+  "GCLOUD_PROJECT": "cit-node",
+  "DATA_BACKEND": "cloudsql",
+  "MYSQL_USER": "root",
+  "MYSQL_PASSWORD": "L00k4ugood!",
+  "INSTANCE_CONNECTION_NAME": "cit-node:us-central1:cit-db"
 });
 
 con.connect(function(err) {
