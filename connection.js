02f7d79dd92397d89c718ca85d2f27e4fb15a475
Tom Hutton <thomaswkhutton@gmail.com>
2018-12-14 13:41:45 -0500

    MySQL changes.

--- connection.js
+++ connection.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 var con = mysql.createConnection({
   host: "gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user: "d56v50k1rt7gaqxf",
+  port: 3306,
   password: "molx3uajbokigj3k",
   database: "mjmy75cwqsv9nnyw"
 });
